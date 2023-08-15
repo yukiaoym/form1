649b35c0c84c6f2302b8f24c6fd21bb82fb409c9
yukiaoym <y.a.102717@gmail.com>
2023-08-15 12:07:09 +0900

    for vercel

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-const SUB_DIRECTORY = "/form";
+// const SUB_DIRECTORY = "/form";
+const SUB_DIRECTORY = "";
 const isProd = process.env.NODE_ENV == "production"
 
 module.exports = {
