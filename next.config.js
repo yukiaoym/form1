8d31adaf855df0bdee3a841166589c373592d462
yukiaoym <y.a.102717@gmail.com>
2023-08-15 13:57:23 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-// const SUB_DIRECTORY = "/form";
-const SUB_DIRECTORY = "";
+const SUB_DIRECTORY = "/form";
+// const SUB_DIRECTORY = "";
 const isProd = process.env.NODE_ENV == "production"
 
 module.exports = {
   nextConfig,
-  basePath: isProd ? SUB_DIRECTORY : "",
+  // basePath: isProd ? SUB_DIRECTORY : "",
   compiler: {
     styledComponents: true,
   },
